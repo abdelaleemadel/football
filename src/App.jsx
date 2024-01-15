22af205ea1bc80435402195e60fa76f031023196
abdelaleemadel <ahmedadel2912@gmail.com>
2024-01-15 12:39:59 +0200

    Fix path error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.scss';
 import Home from './components/home/home';
 import Layout from './components/Layout/Layout';
-import Countries from './components/countries/Countries';
+import Countries from './components/Countries/Countries';
 import Players from './components/Players/Players';
 
 const routers = createBrowserRouter([
