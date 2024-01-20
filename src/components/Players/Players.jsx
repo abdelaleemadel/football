f49bd6fb09bbb5c9234123e6e79c8945ce85fa43
abdelaleemadel <ahmedadel2912@gmail.com>
2024-01-20 21:34:13 +0200

    Move the players folder to the public folder to fetch it using axios

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
-import allPlayers from 'favicon';
+/* import * as allPlayers from '../../assets/players'; */
 import Player from "../Player/Player";
 import { useEffect, useState } from "react";
 
@@ -20,21 +20,21 @@ function Players() {
 
 
     function getPlayers() {
-        console.log(allPlayers);
-        return fetch('./../../assets/players/Brazil.txt').then(response => response.text());
+
+        return axios.get('/players/Germany.txt');
     }
 
     let { isLoading, isError, data } = useQuery('allPlayers', getPlayers);
-    /*     let playersByCountry = data?.data.slice(0, 20);
-     */    /* console.log(data); */
-    /*  return (<>
-         <div className="main-bg main-vh-100">
-             <div className="container row mx-auto chess">
-                 {playersByCountry?.map(player => <div className="col-6 col-md-4 col-lg-3 py-4 d-flex flex-wrap justify-content-center align-items-center" key={player.player_id}> <Player player={player}></Player></div>)}
-             </div>
- 
-         </div>
-     </>); */
+    let playersByCountry = data?.data.slice(0, 20);
+    console.log(playersByCountry);
+    return (<>
+        <div className="main-bg main-vh-100">
+            <div className="container row mx-auto chess">
+                {playersByCountry?.map(player => <div className="col-6 col-md-4 col-lg-3 py-4 d-flex flex-wrap justify-content-center align-items-center" key={player.player_id}> <Player player={player}></Player></div>)}
+            </div>
+
+        </div>
+    </>);
 }
 
 export default Players;
