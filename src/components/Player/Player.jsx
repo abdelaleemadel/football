05a952361a7801390923e0c98f651f06807d509d
abdelaleemadel <ahmedadel2912@gmail.com>
2024-01-20 18:33:39 +0200

    Remove Germany players to update them

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -5,7 +5,7 @@ export default function Player({ player }) {
 
 
         {player["image_path"] ? <img src={player["image_path"]} alt={player.name} className='w-50' /> : ''}
-        <p className='text-center mx-auto text-white fw-bold fs-3 mb-0 w-100'>{player.name}</p>
+        <p className='text-center mx-auto text-white fw-bold fs-3 mb-0 w-100'>{player.common_name}</p>
         <p className='text-center mx-auto text-white fw-bold fs-3 mb-0 w-100'>{player.date_of_birth}</p>
     </>
 
--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
-import allPlayers from './players.txt';
+import allPlayers from './../../assets/players/Brazil.txt';
 import Player from "../Player/Player";
 
 function Players() {
@@ -10,12 +10,12 @@ function Players() {
         return axios.get(allPlayers);
     }
     let { isLoading, isError, data } = useQuery('allPlayers', getPlayers);
-    let playersByCountry = data?.data.filter(player => player.country_id == countryId);
+    let playersByCountry = data?.data.slice(0, 50);
     console.log(playersByCountry);
     return (<>
         <div className="main-bg main-vh-100">
             <div className="container row mx-auto chess">
-                {playersByCountry?.map(player => <div className="col-6 col-md-4 col-lg-3 py-4 d-flex flex-wrap justify-content-center align-items-center " key={player.id}> <Player player={player}></Player></div>)}
+                {playersByCountry?.map(player => <div className="col-6 col-md-4 col-lg-3 py-4 d-flex flex-wrap justify-content-center align-items-center" key={player.player_id}> <Player player={player}></Player></div>)}
             </div>
 
         </div>
